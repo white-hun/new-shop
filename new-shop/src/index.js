28cfc7cf7ac9e04e0fb9723eb0bf02a039f89ccd
white-hun <121800818+white-hun@users.noreply.github.com>
2023-06-20 17:27:50 +0900

    Update react router

--- new-shop/src/index.js
+++ new-shop/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import MyCart from "./pages/MyCart";
+import AllProducts from "./pages/AllProducts";
+import NewProduct from "./pages/NewProduct";
+import ProductDetail from "./pages/ProductDetail";
+import MyCart from "./pages/MyCart";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <NotFound />,
+    element: <App />,
+    children: [
+      { index: true, path: "/", element: <Home /> },
+      { path: "/products", element: <AllProducts /> },
+      { path: "/product/new", element: <NewProduct /> },
+      { path: "/product/:id", element: <ProductDetail /> },
+      { path: "/carts", element: <MyCart /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <RouterProvider router={router} />
+  // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ new-shop/src/pages/AllProducts.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Allproducts() {
+  return <div>All Products</div>;
+}
